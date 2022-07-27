79543dcd9ce61819e37e7b241d884a4afcb4b2fc
wzrdk3lly <dkmk1149@gmail.com>
2022-07-26 21:48:28 -0400

    ADD functionality to add new books by clicking "+"
    ADD updates to Book card layout
    
    FIX word overflow on card by removing "h-screen" class

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 
 let library = [];
 
-function Book(id, title, author, isRead){
-    this.id = id
-    this.title = title
-    this.author = author 
-    this.isRead = isRead
-}
-
-// Query selectors
-
+// global query selectors
 let bookSection = document.getElementById("book-section")
-// add code for deleting a card...if delete card run refresh function to assign cards with new id's
+let btnAddBook = document.getElementById("add-book")
+
+
+
+// add event listener for deleting a card...if delete card run refresh function to assign cards with new id's
 // don't allow for edit capabilities -- remove pencil button
 // focus on adding and deleting cards, then you can focus on updating with read or not
 
+
+//Event listener for adding book to library
+btnAddBook.addEventListener("click", addBookToLibrary)
+
 function addBookToLibrary(){
     let toggle = true;
-   let id = generateID();
-   let title = prompt("What is the title of the book:  ");
+    let id = generateID();
+    let title = prompt("What is the title of the book:  ");
     let author = prompt("What is the author of the book: ");
     let isRead = prompt("Have you read the the book: y/n");
 
@@ -40,8 +40,16 @@ function addBookToLibrary(){
     let newBook = new Book(id, title, author, isRead);
   
     library.push(newBook)
+    displayBook();
     
     return newBook
+}
+
+function Book(id, title, author, isRead){
+    this.id = id
+    this.title = title
+    this.author = author 
+    this.isRead = isRead
 }
 
 function generateID(){
@@ -53,50 +61,65 @@ function generateID(){
     }
 }
 
+// add an updateID() function that gives that iterates over each book and gives the book a new id
+
 
 function displayBook(){
-    // for(book in library){
-    //     console.log(book.title)
-    //     // let newBook = document.createElement("div");
-    //     // newBook.text = "book.title + book.author + book.isRead"
-    //     // bookSection.appendChild(newBook)
-    // }   
+
+    bookSection.innerHTML = "";
 
     library.forEach(function (bookObject){
-        let newBook = document.createElement("div");
-        newBook.innerText = bookObject.title;
+
+        // Refactor to determineReadElements - include in addcard function
+        let btnTitle = "";
+        result = ""
+        if(bookObject.isRead){
+            btnTitle = "Not Read"
+            result = "Read"
+        }
+        else{
+            btnTitle = "Read"
+            result = "Not Read"
+        }
+
+        // Refactor to addcard function
+        let newBookCard = document.createElement("div");
+        newBookCard.classList.add("bg-white", "rounded-lg", "pl-2", "space-y-4")
+
+        let titleHeader = document.createElement("h5");
+        titleHeader.classList.add("text-xl", "font-bold");
+        titleHeader.innerText = "Title: " + bookObject.title;
+
+        let divAuthor = document.createElement("div");
+        divAuthor.innerText = "Author: " + bookObject.author
+
+        let divRead = document.createElement("div");
+        divRead.innerText = result
+
+        let divBtns = document.createElement("div");
+        divBtns.classList.add("space-x-2")
+
+        let btnDelete = document.createElement("button")
+        btnDelete.classList.add("focus:outline-none", "text-white", "bg-purple-700", "hover:bg-purple-800", 
+                                     "font-medium", "rounded-lg", "text-sm", "px-5", "py-2.5", 
+                                     "mb-2", "dark:bg-purple-600", "dark:hover:bg-purple-700");
+        btnDelete.innerText = "Delete"
+
+        let btnRead = document.createElement("button");
+        btnRead.classList.add("focus:outline-none", "text-white", "bg-purple-700", "hover:bg-purple-800",  
+                                "font-medium", "rounded-lg", "text-sm", "px-5", "py-2.5", "mb-2", 
+                                "dark:bg-purple-600", "dark:hover:bg-purple-700");
+        btnRead.innerText = btnTitle
+
+        divBtns.appendChild(btnDelete)
+        divBtns.appendChild(btnRead)
+        newBookCard.appendChild(titleHeader)
+        newBookCard.appendChild(divAuthor)
+        newBookCard.appendChild(divRead)
+        newBookCard.appendChild(divBtns)
+       
+        bookSection.appendChild(newBookCard)
        
     });
 }
 
-// $('#book-section').append(`
-// <div class="p-4 flex flex-col border-4 rounded-lg shadow-lg bg-white w-52 ">
-//   <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">${bookObject.title}</h5>
-//   <p class="text-gray-700 text-base mb-4">
-//     ${bookObject.author}
-//   </p>
-//   <p class="text-gray-700 text-base mb-4">
-//   ${bookObject.isRead}
-//   </p>
- 
-//   <div class="flex flex-row gap-10 self-center">
-//     <a href="">
-//         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6  hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-//             <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-//           </svg>
-//       </a>
-
-//       <a href="">
-//         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6  hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-//             <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-//           </svg>
-//       </a>
-//   </div>
-// </div>`)
-
-
-
-
-// Test book
-library.push((new Book("life of miles", "miles", true)))
-// console.log(library)
