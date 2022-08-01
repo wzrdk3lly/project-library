e2898a35d81bd5e7c73f68631a3c52b10777dcd7
wzrdk3lly <dkmk1149@gmail.com>
2022-07-31 23:15:17 -0400

    ADD Delete functionality from UI ADD Delete functionality in "back end" library array

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@
 let library = [];
 
 // global & static query selectors
-let bookSection = document.getElementById("book-section")
-let btnAddBook = document.getElementById("add-book")
-let overlay = document.getElementById("overlay")
-let btnExitOverlay = document.getElementById("btn-exit")
+let bookSection = document.getElementById("book-section");
+let btnAddBook = document.getElementById("add-book");
+let overlay = document.getElementById("overlay");
+let btnExitOverlay = document.getElementById("btn-exit");
+let btnDelete = document.getElementsByClassName("delete");
 
-//Event listener section
 
 // add event listener for deleting a card...if delete card run refresh function to assign cards with new id's
+
+btnCard = document.ge
 // don't allow for edit capabilities -- remove pencil button
 // focus on adding and deleting cards, then you can focus on updating with read or not
 
 //Event listener for adding book to library
-// btnAddBook.addEventListener("click", addBookToLibrary) // paused for overlay functionality
-
    btnAddBook.addEventListener("click",() => {
         overlay.classList.remove("hidden")
         overlay.classList.add("flex")
@@ -48,8 +48,10 @@ function addBookToLibrary(title,author, isRead){
     let id = generateID();
 
     let newBook = new Book(id, title, author, isRead);
-  
+    
     library.push(newBook)
+    console.log(`adding: new book to display with id:${newBook.id} title:${newBook.title} 
+                 author: ${newBook.author} and read:${newBook.isRead} to display`)
     displayBook();
     
     return newBook
@@ -64,6 +66,23 @@ function generateID(){
     }
 }
 
+// Deletes book from library depending on the bookID passed in
+function deleteBookFromLibrary(id){
+    // Remove specified ID 
+    if (library.length > 1) {
+       library.splice(id-1,1); 
+    }
+    else{
+        library.pop();
+    }
+    
+    //updateID need to create a refresh function to iterate through library and update with new id's
+    library.forEach(function(x,index){
+        x.id = index + 1
+    })
+    
+}
+
 function Book(id, title, author, isRead){
     this.id = id
     this.title = title
@@ -71,7 +90,6 @@ function Book(id, title, author, isRead){
     this.isRead = isRead
 }
 
-// add an updateID() function that gives that iterates over each book and gives the book a new id
 function displayBook(){
 
     //Prevents duplication of displaying books
@@ -89,7 +107,9 @@ function removeOverlay(){
 
 
 function addCard(bookObject){
-
+    //add a card id thats hidden. Whenever I click submit I can grab that 
+    //card Id instead of trying to reference the object I originally got the data from
+    id = bookObject.id;
     // Create card elements
     let newBookCard = document.createElement("div");
     let titleHeader = document.createElement("h5");
@@ -101,9 +121,10 @@ function addCard(bookObject){
 
     //Style card elements
     newBookCard.classList.add("bg-white", "rounded-lg", "pl-2", "space-y-4");
+    newBookCard.setAttribute("id",`card${bookObject.id}`)
     titleHeader.classList.add("text-xl", "font-bold");
     divBtns.classList.add("space-x-2");
-    btnDelete.classList.add("focus:outline-none", "text-white", "bg-purple-700", "hover:bg-purple-800", 
+    btnDelete.classList.add("delete", "focus:outline-none", "text-white", "bg-purple-700", "hover:bg-purple-800", 
                             "font-medium", "rounded-lg", "text-sm", "px-5", "py-2.5", 
                             "mb-2", "dark:bg-purple-600", "dark:hover:bg-purple-700");
     btnRead.classList.add("focus:outline-none", "text-white", "bg-purple-700", "hover:bg-purple-800",  
@@ -127,6 +148,17 @@ function addCard(bookObject){
    
     //Add card to dashboard
     bookSection.appendChild(newBookCard)
+
+    //Add event listeners for the delete button on each card
+    btnDelete.addEventListener("click", function(){
+    
+        //Removes the whole card from the front end when the delete button is selected
+        newBookCard.remove();
+    
+        //call the delete book function
+        deleteBookFromLibrary(id)
+
+    })
 }
 
 // The read/not read button needs to say the opposite for toggle functionality
